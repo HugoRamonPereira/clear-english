3de2ea1cc6e762465f0123e0779d3f536f7c06e6
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-03-26 11:20:06 -0300

    Added Tailwind and Shadcn

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,9 +1,27 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+</script>
+
 <template>
-  <header>
-    Component: TheHeader
-  </header>
+  <div class='w-full flex items-center justify-between'>
+    <h2>Clear English</h2>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    <div>Login Info</div>
+  </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style scoped></style>
