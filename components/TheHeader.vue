5573435d37d46e2ec4ece0ad1ab84ebcf01aad06
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-03-29 22:19:23 -0300

    Styled header, its navigation options and buttons

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,4 +1,73 @@
+<template>
+  <div class='w-full flex items-center justify-between h-16'>
+    <p class='font-normal'>Clear English</p>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
+              <li class="row-span-3">
+                <NavigationMenuLink as-child>
+                  <a class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/">
+                    <img src="https://www.radix-vue.com/logo.svg" class="h-6 w-6">
+                    <div class="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p class="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              <ListItem v-for="component in components" :key="component.title" :title="component.title"
+                :href="component.href">
+                {{ component.description }}
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/docs" :class="navigationMenuTriggerStyle()">
+            Documentation
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    <div class='font-light flex gap-4'>
+      <Button class='flex gap-1 bg-violet-500 text-white py-1 px-4 rounded-lg hover:bg-violet-600'>
+        Sign in
+        <LogIn :size="18" :stroke-width="1.5" />
+      </Button>
+      <Button
+        class='flex gap-1 bg-white text-black border border-black py-1 px-4 rounded-lg hover:bg-slate-800 hover:text-white'>
+        Sign up
+        <UserPlus :size="18" :stroke-width="1.5" />
+      </Button>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -6,36 +75,47 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
 import { Button } from '@/components/ui/button'
 import { LogIn, UserPlus } from 'lucide-vue-next';
 
-</script>
+const components: { title: string, href: string, description: string }[] = [
+  {
+    title: 'Alert Dialog',
+    href: '/docs/primitives/alert-dialog',
+    description:
+      'A modal dialog that interrupts the user with important content and expects a response.',
+  },
+  {
+    title: 'Hover Card',
+    href: '/docs/primitives/hover-card',
+    description:
+      'For sighted users to preview content available behind a link.',
+  },
+  {
+    title: 'Progress',
+    href: '/docs/primitives/progress',
+    description:
+      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+  },
+  {
+    title: 'Scroll-area',
+    href: '/docs/primitives/scroll-area',
+    description: 'Visually or semantically separates content.',
+  },
+  {
+    title: 'Tabs',
+    href: '/docs/primitives/tabs',
+    description:
+      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+  },
+  {
+    title: 'Tooltip',
+    href: '/docs/primitives/tooltip',
+    description:
+      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+  },
+]
 
-<template>
-  <div class='w-full flex items-center justify-between h-16'>
-    <p class='font-normal'>Clear English</p>
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-    <div class='font-light flex gap-4'>
-      <Button
-        class='flex gap-1 bg-white border border-violet-500 text-violet-500 py-1 px-4 rounded-lg hover:bg-violet-500 hover:text-white'>
-        Sign in
-        <LogIn :size="18" :stroke-width="1.5" />
-      </Button>
-      <Button
-        class='flex gap-1 bg-white text-black border border-black py-1 px-4 rounded-lg hover:bg-black hover:text-white'>
-        Sign up
-        <UserPlus :size="18" :stroke-width="1.5" />
-      </Button>
-    </div>
-  </div>
-</template>
+</script>
