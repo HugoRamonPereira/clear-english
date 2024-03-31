1cc19593b3ac2599d1a8012bb5eda20569e130d0
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-03-30 23:29:24 -0300

    Added search, command, dialog and its functionalities

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,80 +2,87 @@
   <header class='w-full border-b border-b-stone-300'>
     <div class='max-w-[1200px] flex items-center justify-center mx-auto'>
       <div class='w-full flex items-center justify-between h-16'>
-        <NuxtLink to='/'>
-          <p class='font-normal'>Clear English</p>
-        </NuxtLink>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
-                  <li class="row-span-3">
-                    <NavigationMenuLink as-child>
-                      <a class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/">
-                        <img src="https://www.radix-vue.com/logo.svg" class="h-6 w-6">
-                        <div class="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p class="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components built with Radix UI and
-                          Tailwind CSS.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href="/docs/primitives/typography" title="Typography">
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  <ListItem v-for="component in components" :key="component.title" :title="component.title"
-                    :href="component.href">
-                    {{ component.description }}
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/docs" :class="navigationMenuTriggerStyle()">
-                <NuxtLink to='/membership' class='flex gap-1'>
-                  Go Premium
-                  <Crown :size="18" :stroke-width="1.5" />
-                </NuxtLink>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div>
-          <ThemeSwitcher />
+        <div class='flex items-center gap-14'>
+          <NuxtLink to='/'>
+            <p class='font-normal'>Clear English</p>
+          </NuxtLink>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
+                    <li class="row-span-3">
+                      <NavigationMenuLink as-child>
+                        <a class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                          href="/">
+                          <img src="https://www.radix-vue.com/logo.svg" class="h-6 w-6">
+                          <div class="mb-2 mt-4 text-lg font-medium">
+                            shadcn/ui
+                          </div>
+                          <p class="text-sm leading-tight text-muted-foreground">
+                            Beautifully designed components built with Radix UI and
+                            Tailwind CSS.
+                          </p>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                    <ListItem href="/docs" title="Introduction">
+                      Re-usable components built using Radix UI and Tailwind CSS.
+                    </ListItem>
+                    <ListItem href="/docs/installation" title="Installation">
+                      How to install dependencies and structure your app.
+                    </ListItem>
+                    <ListItem href="/docs/primitives/typography" title="Typography">
+                      Styles for headings, paragraphs, lists...etc
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    <ListItem v-for="component in components" :key="component.title" :title="component.title"
+                      :href="component.href">
+                      {{ component.description }}
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/docs" :class="navigationMenuTriggerStyle()">
+                  <NuxtLink to='/membership' class='flex gap-1'>
+                    Go Premium
+                    <Crown :size="18" :stroke-width="1.5" />
+                  </NuxtLink>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
-        <div class='font-light flex gap-4'>
-          <Button class='flex gap-1 bg-violet-500 text-white py-1 px-4 rounded-lg hover:bg-violet-600'>
-            <NuxtLink to='signin'>
-              Sign in
-            </NuxtLink>
-            <LogIn :size="18" :stroke-width="1.5" />
-          </Button>
-          <Button
-            class='flex gap-1 bg-white text-black border border-black py-1 px-4 rounded-lg hover:bg-slate-800 hover:text-white'>
-            <NuxtLink to='signup'>
-              Sign up
-            </NuxtLink>
-            <UserPlus :size="18" :stroke-width="1.5" />
-          </Button>
+        <div class='flex items-center gap-4'>
+          <div>
+            <TheSearch />
+          </div>
+          <div>
+            <ThemeSwitcher />
+          </div>
+          <div class='font-light flex gap-4'>
+            <Button class='flex gap-1 bg-violet-500 text-white py-1 px-4 rounded-lg hover:bg-violet-600'>
+              <NuxtLink to='signin'>
+                Sign in
+              </NuxtLink>
+              <LogIn :size="18" :stroke-width="1.5" />
+            </Button>
+            <Button
+              class='flex gap-1 bg-white text-black border border-black py-1 px-4 rounded-lg hover:bg-slate-800 hover:text-white'>
+              <NuxtLink to='signup'>
+                Sign up
+              </NuxtLink>
+              <UserPlus :size="18" :stroke-width="1.5" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/TheSearch.vue
@@ -0,0 +1,82 @@
+<template>
+  <Dialog>
+    <DialogTrigger>
+      <Button variant='outline' class='w-[220px] flex justify-between' @click='handleOpenChange'>
+        Search website...
+        <div class='flex items-center gap-0.5 text-xs border border-1 p-1 px-1.5 -mr-2 rounded-md bg-gray-50'>
+          <Command :size="11" :stroke-width="2" />
+          K
+        </div>
+      </Button>
+    </DialogTrigger>
+  </Dialog>
+  <CommandDialog v-model:open="open">
+    <CommandInput placeholder="Type a command or search..." class='pr-8' />
+    <CommandList>
+      <CommandEmpty>No results found.</CommandEmpty>
+      <CommandGroup heading="Suggestions">
+        <CommandItem value="courses" class='flex items-center gap-1.5'>
+          <NotebookPen :size="18" :stroke-width="1.5" />
+          Courses
+        </CommandItem>
+        <CommandItem value="exercises" class='flex items-center gap-1.5'>
+          <BookOpenText :size="18" :stroke-width="1.5" />
+          Exercises
+        </CommandItem>
+        <CommandItem value="listening" class='flex items-center gap-1.5'>
+          <Headphones :size="18" :stroke-width="1.5" />
+          Listening
+        </CommandItem>
+      </CommandGroup>
+      <CommandSeparator />
+      <CommandGroup heading="Settings">
+        <CommandItem value="profile" class='flex items-center gap-1.5'>
+          <User :size="18" :stroke-width="1.5" />
+          Profile
+        </CommandItem>
+        <CommandItem value="billing" class='flex items-center gap-1.5'>
+          <CircleDollarSign :size="18" :stroke-width="1.5" />
+          Billing
+        </CommandItem>
+        <CommandItem value="settings" class='flex items-center gap-1.5'>
+          <Settings :size="18" :stroke-width="1.5" />
+          Settings
+        </CommandItem>
+      </CommandGroup>
+    </CommandList>
+  </CommandDialog>
+</template>
+
+<script setup lang="ts">
+import { useMagicKeys } from '@vueuse/core'
+import { Command, NotebookPen, BookOpenText, Headphones, User, CircleDollarSign, Settings } from 'lucide-vue-next'
+import { ref, watch } from 'vue'
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command'
+
+const open = ref(false)
+
+const { Meta_K, Ctrl_K } = useMagicKeys({
+  passive: false,
+  onEventFired(e) {
+    if (e.key === 'k' && (e.metaKey || e.ctrlKey))
+      e.preventDefault()
+  },
+})
+
+watch([Meta_K, Ctrl_K], (v) => {
+  if (v[0] || v[1])
+    handleOpenChange()
+})
+
+function handleOpenChange() {
+  open.value = !open.value
+}
+</script>
