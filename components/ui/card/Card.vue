a8ef0aebe206d6555487ace90fccb8b38a049c77
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-09-11 15:11:46 -0300

    Final touches in the membership page

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-lg border bg-card text-card-foreground shadow-sm hover:border-violet-600 hover:-translate-y-2 hover:transition-transform hover:duration-300 cursor-pointer transition-colors duration-500',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
--- pages/membership.vue
+++ pages/membership.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <p>Page: membership</p>
-    <p>These are the advantages of being a Premium Member</p>
+  <div class="flex flex-col sm:h-[calc(100vh-172px)] justify-center">
+    <div class="flex flex-col gap-6 text-center py-10 px-10 sm:px-0">
+      <p class="text-4xl font-bold text-gray-800">
+        Do you want to become a member?
+      </p>
+      <p class="tracking-wide">
+        Check our plans below and choose the one that best fits your needs
+      </p>
+    </div>
+    <div
+      class="flex flex-col sm:flex-row items-center justify-evenly gap-6 mt-4"
+    >
+      <ThePlanCard title="Basic" description="Restricted Access" :price="0" />
+      <ThePlanCard
+        title="Lifetime"
+        description="Access to everything forever"
+        :price="500"
+      />
+      <ThePlanCard
+        title="Monthly"
+        description="Access to everything for as long as you pay"
+        :price="20"
+      />
+    </div>
+    <div class="mt-14 w-fit mx-auto justify-center">
+      <NuxtLink
+        to="/"
+        class="flex items-center gap-2 justify-center border border-gray-300 rounded-md px-4 h-10 text-gray-400 hover:text-gray-500 hover:border-gray-400 hover:bg-gray-100/50 hover:trasnsition-colors hover:duration-200 dark:hover:bg-gray-900/50 dark:border-gray-500/50 dark:text-gray-500 focus-visible:outline-2 focus-visible:outline-violet-violetMain"
+      >
+        Back to Home
+        <Home :size="18" :stroke-width="1.5" />
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Home } from "lucide-vue-next";
+
 useHead({
   title: "Membership",
 });
